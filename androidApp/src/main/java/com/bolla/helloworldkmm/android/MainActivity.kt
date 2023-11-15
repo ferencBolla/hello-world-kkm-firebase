81ca28a92018913662c0e0cb96bb3fd20ce4b658
ferenc.bolla <bollaferenc@gmail.com>
2023-11-15 02:37:34 +0100

    Fixed user logger button size

--- androidApp/src/main/java/com/bolla/helloworldkmm/android/MainActivity.kt
+++ androidApp/src/main/java/com/bolla/helloworldkmm/android/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.bolla.helloworldkmm.Greeting
@@ -25,12 +28,19 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingView(Greeting().greet())
-                    Button(
-                        onClick = {
-                            testLogUsers()
-                        }) {
-                        Text(text = "Log users to console")
+                    Box(
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Column {
+                            GreetingView(Greeting().greet())
+                            Button(
+                                onClick = {
+                                    testLogUsers()
+                                }
+                            ) {
+                                Text(text = "Log users to logcat")
+                            }
+                        }
                     }
                 }
             }
